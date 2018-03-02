149b5fe5b4f5048aa726efb377bf25e57de84e0d
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-01 21:50:34 -0800

    Invoked fetchPopularRepos to fetch popular repos for a selected language

--- src/Popular.js
+++ src/Popular.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import SelectLanguage from './SelectLanguage';
-import api from './utils/api';
+import {fetchPopularRepos} from './utils/api';
 
 class Popular extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     }
     this.updateLanguage = this.updateLanguage.bind(this);
   }
 
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
   updateLanguage(lang) {
-    this.setState({selectedLanguage: lang})
+    this.setState({selectedLanguage: lang, repos: null})
+    fetchPopularRepos(this.state.selectedLanguage)
+      .then((repos) => this.setState({repos}))
   }
 
   render() {
