f2f817ca78d3d9f1f571e43eebe34732616c30e3
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-01 21:25:23 -0800

    Added selectedLanguage state and handled language updation

--- src/Popular.js
+++ src/Popular.js
@@ -1,14 +1,37 @@
 import React from 'react';
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    }
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState({selectedLanguage: lang})
+  }
+
   render() {
     let languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
+    console.log(this); // Popular
     return (
-      <ul className="languages">
-        {languages.map((lang, index) => <li key={lang}>{lang}</li>)}
+      <ul className='languages'>
+        { languages.map( (lang,index) =>
+          {
+            console.log(this) // Popular
+            return (
+              <li onClick={() =>this.updateLanguage(lang)}
+                  key={lang}
+                  style={ lang === this.state.selectedLanguage ? {color: '#d0021b'} : null} >
+                {lang}
+              </li>
+            )
+          } )
+        }
       </ul>
     )
   }
 }
-
 export default Popular;
