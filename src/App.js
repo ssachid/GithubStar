2ccc27cef03dfb51a4605a3683af493b05c89430
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-03 18:19:20 -0800

    Encapsulated routes with Switch component, Not Found rendering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Popular from './Popular';
+import Battle from './Battle';
+
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
@@ -8,7 +10,12 @@ class App extends React.Component {
         <Router>
           <div className='container'>
             <Nav />
-            <Route path='/popular' component={Popular} />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/battle" component={Battle} />
+              <Route path='/popular' component={Popular} />
+              <Route render={() => <p> Not Found </p>} />
+            </Switch>
           </div>
         </Router>
       )
