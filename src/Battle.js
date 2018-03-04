f3872c7a4b5f2bf95a2dccdcdf7ecf50e7072bc3
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-03 18:47:27 -0800

    Added PlayerInput component invocation in Battle component

--- src/Battle.js
+++ src/Battle.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
 class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   render() {
+    const { playerOneName, playerTwoName } = this.state;
     return (
       <div>
-        BATTLE!
+        <div className="row">
+          { !playerOneName &&
+            <PlayerInput
+              id="playerOne"
+              label="Player One"
+              onSubmit={this.handleSubmit}
+          }
+
+         { !playerTwoName &&
+            <PlayerInput
+              id="playerTwo"
+              label="Player Two"
+              onSubmit={this.handleSubmit}
+          }
+        </div>
       </div>
     )
   }
